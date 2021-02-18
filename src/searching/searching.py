fee4e0e25132fee7862d5b76f7735c3f41c0bc9b
s-munro <68410363+s-munro@users.noreply.github.com>
2021-02-17 21:08:12 -0500

    binary search notes

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,27 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, start, end):
+def binary_search(arr, target, start = None, end = None):
     # Your code here
+    if start is None:
+        start = 0
+    if end is None:
+        end = len(arr)
+
+    if start > end:
+        print("sorry get out of here")
+        return -1
+
+
+    middle = (start + end) // 2
+    if arr[middle] == target:
+        return middle
+        
+    elif arr[middle] < target: # arr less than target, so move right
+        return binary_search(arr, target, middle + 1, end)
+    
+    else: # middle is greater than target, so move left
+        return binary_search(arr, target, 0, middle - 1)
+
+        
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +30,24 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
+def agnostic_binary_search(arr, target, start = None, end = None):
+    if start is None:
+        start = 0
+    if end is None:
+        end = len(arr)
+    
+    if start > end:
+        print("no way jose")
+        return -1
+    
+    middle = (start + end) // 2
+    if arr[middle] == target:
+        return middle
+    
+    elif arr[middle] < target:
+        return binary_search(arr, target, middle + 1, end)
+    
+    else:
+        if
     # Your code here
 
